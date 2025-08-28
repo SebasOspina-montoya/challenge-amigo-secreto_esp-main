4c55c87c0ba86086e553ae9f3c2f99d308682caf
SebasOspina-montoya <ospinaseosmo@hotmail.com>
2025-08-28 16:50:53 -0500

    actualizacion codigo java funcion agregar amigo y añadir listas

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let nombreAmigos = [];
+
+console.log(nombreAmigos);
+
+function agregarAmigo() {
+    let nombrePersona = document.getElementById('amigo').value;
+
+    if (nombrePersona === ""){
+        alert("Por favor, inserte un nombre");
+    } else{
+        nombreAmigos.push(nombrePersona);
+    }
+    añadirlista();
+    limpiarCaja();
+
+}
+
+function limpiarCaja () {
+    document.querySelector ('#amigo').value = '';
+}
+
+function añadirlista () {
+  let lista = document.getElementById('listaAmigos');
+  lista.innerHTML = ''; // Limpiar la lista antes de añadir elementos
+
+  for (let i = 0; i < nombreAmigos.length; i++) {
+    let amigo = nombreAmigos[i];
+    let listItem = document.createElement('li');
+    listItem.textContent = amigo;
+    lista.appendChild(listItem);
+  }
+}
